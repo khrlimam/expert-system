b3a1c8b21084997cd5ae3891c183b41a9f15674e
Khairul Imam <ki65559@gmail.com>
2018-08-04 17:25:34 +0800

    add heroku config var

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/tht_expert'
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 THREADS_PER_PAGE = 2
